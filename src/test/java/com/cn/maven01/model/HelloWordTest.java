5b4fe580e7337233f3849cf3ecbea1e89706e916
bskycool <46081268+bskycool@users.noreply.github.com>
2018-12-23 14:48:31 +0800

    Create test/java/com/cn/maven01/model/HelloWordTest.java

---
+++ src/test/java/com/cn/maven01/model/HelloWordTest.java
@@ -0,0 +1,5 @@
+package com.cn.maven01.model;
+import org.junit.*;
+import org.junit.Assert.*;
+public class HelloWordTest{@Testpublic void testHello(){Assert.assertEquals("HelloWord!",new HelloWord().sayHello());}
+}
