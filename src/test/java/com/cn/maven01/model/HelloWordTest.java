afd99727d02e95dfce4aa19a30f7ee7812f1febc
bskycool <46081268+bskycool@users.noreply.github.com>
2018-12-23 15:31:21 +0800

    Update HelloWordTest.java

--- src/test/java/com/cn/maven01/model/HelloWordTest.java
+++ src/test/java/com/cn/maven01/model/HelloWordTest.java
@@ -1,5 +1,9 @@
+
 package com.cn.maven01.model;
 import org.junit.*;
 import org.junit.Assert.*;
-public class HelloWordTest{@Test public void testHello(){Assert.assertEquals("HelloWord!",new HelloWord().sayHello());}
+public class HelloWordTest{
+  @Test 
+  public void testHello(){
+    Assert.assertEquals("HelloWord!",new HelloWord().sayHello());}
 }
